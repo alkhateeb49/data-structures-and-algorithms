81ed374d282f818cf0beab01989d5ca28a98b4e3
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2021-04-05 17:01:25 +0300

    array shift

--- challenges/test/ArrayShiftTest.java
+++ challenges/test/ArrayShiftTest.java
@@ -1,18 +1,18 @@
 public class ArrayShiftTest {
 
-  @Test public void testArrayShift_evenLengthArray() {
+  @Test public void testArrayShift1() {
     int[] Arr = new int[]{2,4,6,8};
     int[] outArr = new int[]{2,4,5,6,8};
     assertArrayEquals("even length", outArr, ArrayShift.insertShiftArray(Arr, 5));
   }
 
-  @Test public void testArrayShift_oddLengthArray() {
+  @Test public void testArrayShift2() {
     int[] Arr2 = new int[]{4,8,15,23,42};
     int[] outArr2 = new int[]{4,8,15,16,23,42};
     assertArrayEquals("odd length", outArr2, ArrayShift.insertShiftArray(Arr2, 16));
   }
 
-  @Test public void testArrayShift_emptyArray(){
+  @Test public void testArrayShift3(){
     assertArrayEquals("empty array", new int[]{1}, ArrayShift.insertShiftArray(new int[]{}, 1));
   }
 }
