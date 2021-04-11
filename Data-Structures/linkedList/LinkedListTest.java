c034e28333997692493382cb069cf4fd5b41e473
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2021-04-11 22:41:28 +0300

    ll-insertions

--- Data-Structures/linkedList/LinkedListTest.java
+++ Data-Structures/linkedList/LinkedListTest.java
@@ -23,4 +23,43 @@ public class listTest {
         l.insert(9);
         assertTrue("the output is", l.includes(5));
     }
+
+// ----------------------------------------------------------
+
+
+@Test
+public void testAppend() {
+    LinkedList test = new LinkedList();
+    n.insert(2);
+    n.append(14);
+    n.append(1);
+    String expicted1 = "{ 2 } ->{ 14 } ->{ 1 } ->NULL";
+    assertEquals("the output is", expicted1, test.toString());
+}
+
+@Test
+public void testAddBefore() {
+    LinkedList test = new LinkedList();
+    n.insert(14);
+    n.insert(7);
+    n.insert(4);
+    n.insert(6);
+    n.addBefore(14, 2);
+    String expicted1 = "{ 6 } ->{ 4 } ->{ 7 } ->{ 2 } ->{ 14 } ->NULL";
+    assertEquals("the output is", expicted1,test.toString());
+}
+
+@Test
+public void testAddAfter() {
+    LinkedList test = new LinkedList();
+    n.insert(2);
+    n.insert(5);
+    n.insert(4);
+    n.insert(10);
+    n.addAfter(10, 3);
+    String expicted1 = "{ 10 } ->{ 3 } ->{ 4 } ->{ 5 } ->{ 2 } ->NULL";
+    assertEquals("the output is", expicted1, test.toString());
+}
+
+
 }
